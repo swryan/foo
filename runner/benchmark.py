aa0d6b552273d919e9b076aa8c448fa8444c7cb2
swryan <stephen.w.ryan@gmail.com>
2018-02-12 10:23:35 -0500

    handle case of no benchmarks defined

--- runner/benchmark.py
+++ runner/benchmark.py
@@ -1077,8 +1077,13 @@ class BenchmarkRunner(object):
                                 os.remove(csv_file)
 
                 if good_commits:
-                    # back up and transfer database
-                    db.backup()
+                    # if benchmarks didn't fail but there are no commits in database, then
+                    # no benchmarks are defined so don't run again for this set of commits
+                    if not db.get_last_commit(project["repository"]):
+                        write_json(fail_file, current_commits)
+                    else:
+                        # back up and transfer database
+                        db.backup()
 
                 # clean up environment
                 remove_env(run_name, keep_env)
